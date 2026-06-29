75fd258fd5151b92730c3235fce61e63bf26c441
malaxiya20250530-glitch <malaxiya2025053@gmail.com>
2026-06-30 07:01:14 +0800

    🐛 Fix Gradle build: replace setup-gradle action with direct gradle

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -5,8 +5,8 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+@Suppress("UnstableApiUsage")
 dependencyResolutionManagement {
-    @Suppress("UnstableApiUsage")
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
